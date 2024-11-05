6de4f4bbf0d9a8c0b68eb9c2a8cd6b41b5770666
Nabil <noufnouf703@gmail.com>
2024-11-05 16:34:39 +0100

    feat: add login and signup pages with authentication logic

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -1,10 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Navigate} from 'react-router-dom';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
+import useStoreLogin from './store/useStoreLogin';
+import useStoreUser from './store/useStoreUser';
 
+const Logout = () => {
+  const { logout } = useStoreLogin();
+  const { clearUser } = useStoreUser();
+
+  logout();
+  clearUser();
+  return <Navigate to="/login" />;
+}
 const AppRoutes = () => {
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path='/signup' element={<SignUp />}/>
+      <Route path="*" element={<h1>404 - Page not found</h1>} />
     </Routes>
   );
 };
