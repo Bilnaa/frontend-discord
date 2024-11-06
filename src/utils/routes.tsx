a929fe01b3fa0eaa51439e6ab70078ff8a4b81a1
Nabil <noufnouf703@gmail.com>
2024-11-06 14:33:37 +0100

    feat: refactor la gestion de la déconnexion et améliorer l'utilisation des hooks

--- src/utils/routes.tsx
+++ src/utils/routes.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route,Navigate} from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from '../pages/Home';
 import Chat from '../components/home/Chat';
 import Login from '../pages/Login';
@@ -9,38 +10,47 @@ import axios from 'axios';
 
 const Requestlogout = async () => {
   await axios.post("http://localhost:3000/auth/logout", { withCredentials: true })
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.error("Impossible de se déconnecter", error);
-  });
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error("Impossible de se déconnecter", error);
+    });
 };
 
-const Logout =  () => {
+const Logout = () => {
   const { logout } = useStoreLogin();
   const { clearUser } = useStoreUser();
-  logout();
-  clearUser();
-  return <Navigate to="/login" />;
-}
 
-const useAuthenticated = (element : JSX.Element) => {
+  useEffect(() => {
+    const performLogout = async () => {
+      await Requestlogout();
+      logout();
+      clearUser();
+    };
+
+    performLogout();
+  }, [logout, clearUser]);
+
+  return <Navigate to="/login" />;
+};
+
+const useAuthenticated = (element: JSX.Element) => {
   const { isLoggedIn } = useStoreLogin();
   return (
     isLoggedIn ? element : <Navigate to="/login" />
-  )
-}
-const AppRoutes = () => {
+  );
+};
 
+const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={useAuthenticated(<Home />)}>
         <Route path="/chat/:id" element={<Chat />} />
       </Route>
       <Route path="/login" element={<Login />} />
-      <Route path="/logout" element={<Logout />} loader={Requestlogout} />
-      <Route path='/signup' element={<SignUp />}/>
+      <Route path="/logout" element={<Logout />} />
+      <Route path='/signup' element={<SignUp />} />
       <Route path="*" element={<h1>404 - Page not found</h1>} />
     </Routes>
   );
