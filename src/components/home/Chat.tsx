a6180f2ae5f2ba67f275026c7003c313808283c6
JeffY1710 <jeff.yabas@gmail.com>
2024-11-06 16:53:58 +0100

    finished main fonctionnal chat

--- src/components/home/Chat.tsx
+++ src/components/home/Chat.tsx
@@ -55,12 +55,10 @@ function Chat() {
 
     useEffect(() => {
         const fetchMessages = async () => {
-            let temp : [] = []
           await axios.get("http://localhost:3000/messages/" + id, { withCredentials: true })
           .then((response) => {
             clearMessage()
-            setMessage(response.data)  
-            temp = response.data.reverse() 
+            setMessage(response.data.slice().reverse())  
           })
           .catch((error) => {
             console.error(error);
@@ -77,17 +75,15 @@ function Chat() {
   return (
     <div className="chat-container">
         <h3>{currentActiveChat?.username}</h3>
-        <div className="message-zone">
-            <ul>
+            <ul className="message-zone">
                 {messages.map((message) => {
                     if (message.emitterId == id) {
-                        return <li style={{backgroundColor:"rgb(91,75,138,100)"}} key={message.id}>{message.content}</li>
+                        return <li style={{display:"flex", justifyContent:"start" }} key={message.id}><span style={{backgroundColor:"rgb(91,75,138,100)"}}>{message.content}</span></li>
                     }else{
-                        return <li style={{backgroundColor:"rgb(120,88,166,100)"}} key={message.id}>{message.content}</li>
+                        return <li style={{display:"flex", justifyContent:"end" }} key={message.id}><span style={{backgroundColor:"rgb(91,75,138,100)"}}>{message.content}</span></li>
                     }
                 })}
             </ul>
-        </div>
         <div className="inputs">
             <form onSubmit={handleSubmit(onSubmit)} style={{display:"flex"}}>
                 <input className="message-bar" autoComplete="off" type="text" placeholder='Tap your message here' style={{fontSize:'20px'}} {...register("message")}/>
